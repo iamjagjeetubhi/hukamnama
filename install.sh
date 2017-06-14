3f6fd95f5005dd7b63e45fbb3674d8beb2eb4aa9
iamjagjeetubhi <iamjagjeetubhi@gmail.com>
2017-06-14 15:00:03 +0530

    updated imagemagick path

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ apt-get install -y python3-pip && apt-get update
 apt-get install -y python3-bs4
 apt-get install -y imagemagick-common
 cd /usr/share/fonts/truetype/ && wget https://www.sikhnet.com/files/fonts/webakharslim.ttf && chmod 755 webakharslim.ttf
-sed -i 's/<policy domain="path" rights="none" pattern=\"@\*\" \/>/<!-- <policy domain="path" rights="none" pattern=\"@\*\" \/> -->/g' /etc/ImageMagick-6/policy.xml
+sed -i 's/<policy domain="path" rights="none" pattern=\"@\*\" \/>/<!-- <policy domain="path" rights="none" pattern=\"@\*\" \/> -->/g' /etc/ImageMagick/policy.xml
 chmod 755 -R ./*
