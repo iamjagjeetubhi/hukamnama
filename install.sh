e6abf1ef35193cf55dc231e4631a22ef53f9fde7
iamjagjeetubhi <iamjagjeetubhi@gmail.com>
2017-06-14 18:15:38 +0530

    added free-serif font

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ fi
 apt-get install -y python-pip && apt-get update
 apt-get install -y python-bs4
 apt-get install -y imagemagick
-cd /usr/share/fonts/truetype/ && wget https://www.sikhnet.com/files/fonts/webakharslim.ttf && chmod 755 webakharslim.ttf
+cd /usr/share/fonts/truetype/ && wget https://www.sikhnet.com/files/fonts/webakharslim.ttf && chmod 755 webakharslim.ttf 
+wget http://www.fontsaddict.com/download/free-serif.ttf && chmod 755 free-serif.ttf
 sed -i 's/<policy domain="path" rights="none" pattern=\"@\*\" \/>/<!-- <policy domain="path" rights="none" pattern=\"@\*\" \/> -->/g' /etc/ImageMagick-6/policy.xml
 chmod 755 -R ./*
