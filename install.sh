dfcad8fe5c12d189eee2212aaf46dc7157fe2a58
iamjagjeetubhi <iamjagjeetubhi@gmail.com>
2017-06-14 15:24:46 +0530

    updated imagemagick path

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
-apt-get install -y python3-pip && apt-get update
-apt-get install -y python3-bs4
-apt-get install -y imagemagick-common
+apt-get install -y python-pip && apt-get update
+apt-get install -y python-bs4
+apt-get install -y imagemagick
 cd /usr/share/fonts/truetype/ && wget https://www.sikhnet.com/files/fonts/webakharslim.ttf && chmod 755 webakharslim.ttf
 sed -i 's/<policy domain="path" rights="none" pattern=\"@\*\" \/>/<!-- <policy domain="path" rights="none" pattern=\"@\*\" \/> -->/g' /etc/ImageMagick/policy.xml
 chmod 755 -R ./*
