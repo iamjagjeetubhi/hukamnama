e0ae4b7e73754bbde08459ed59903f71b00fa679
iamjagjeetubhi <iamjagjeetubhi@gmail.com>
2017-06-14 17:51:57 +0530

    changed font name

--- hukamnama_gur/hukamnama_gur.sh
+++ hukamnama_gur/hukamnama_gur.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 python hukamnama_gur.py
 #convert -background white -fill black -pointsize 22 -gravity center -font FreeSerif -size 700x caption:@heading.csv hukamnama_heading.png
-convert -background white -fill black -pointsize 22 -gravity center -font WebAkharSlim -stroke black -strokewidth .5 -size 800x caption:@header.csv header.png
-convert -background white -fill black -pointsize 22 -font WebAkharSlim -stroke black -strokewidth .2 -size 800x caption:@paragraph.csv paragraph.png
-convert -background white -fill black -pointsize 22 -gravity center -font WebAkharSlim -stroke black -strokewidth .5 -size 800x caption:@footer.csv footer.png
+convert -background white -fill black -pointsize 22 -gravity center -font WebAkharSlim-Regular -stroke black -strokewidth .5 -size 800x caption:@header.csv header.png
+convert -background white -fill black -pointsize 22 -font WebAkharSlim-Regular -stroke black -strokewidth .2 -size 800x caption:@paragraph.csv paragraph.png
+convert -background white -fill black -pointsize 22 -gravity center -font WebAkharSlim-Regular -stroke black -strokewidth .5 -size 800x caption:@footer.csv footer.png
 convert header.png paragraph.png footer.png -append hukamnama_temp.png
 convert hukamnama_temp.png -bordercolor white -border 15 hukamnama_temp1.png
 convert hukamnama_temp1.png -bordercolor DarkGoldenrod1 -border 10 hukamnama_temp2.png
--- meaning_gur/meaning_gur.sh
+++ meaning_gur/meaning_gur.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 python meaning_gur.py
 #convert -background white -fill black -pointsize 22 -gravity center -font FreeSerif -size 700x caption:@heading.csv hukamnama_heading.png
-convert -background white -fill black -pointsize 22 -gravity center -font WebAkharSlim -stroke black -strokewidth .5 -size 1000x caption:@header.csv header.png
-convert -background white -fill black -pointsize 22 -font WebAkharSlim -stroke black -strokewidth .2 -size 1000x caption:@paragraph.csv paragraph.png
+convert -background white -fill black -pointsize 22 -gravity center -font WebAkharSlim-Regular -stroke black -strokewidth .5 -size 1000x caption:@header.csv header.png
+convert -background white -fill black -pointsize 22 -font WebAkharSlim-Regular -stroke black -strokewidth .2 -size 1000x caption:@paragraph.csv paragraph.png
 convert header.png paragraph.png -append meaning_gur_temp.png
 convert meaning_gur_temp.png -bordercolor white -border 15 meaning_gur_temp1.png
 convert meaning_gur_temp1.png -bordercolor DarkGoldenrod1 -border 10 meaning_gur_temp2.png
