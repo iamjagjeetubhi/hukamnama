f54e9c748f45c58585f1447d69e3b4a576f7d973
iamjagjeetubhi <iamjagjeetubhi@gmail.com>
2017-06-15 15:21:55 +0530

    fixed hukamnama.sh

--- hukamnama.sh
+++ hukamnama.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 cd hukamnama_gur && bash hukamnama_gur.sh && cp hukamnama_gur.png ../ && cd ..
 cd meaning_gur && bash meaning_gur.sh && cp meaning_gur.png ../ && cd ..
 cd hukamnama_eng && bash hukamnama_eng.sh && cp hukamnama_eng.png ../ && cd ..
