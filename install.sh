04c0a5a8b28a043dea0a65ab4bb5d36d3bf5a039
iamjagjeetubhi <iamjagjeetubhi@gmail.com>
2017-06-15 14:08:01 +0530

    corrected permission

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ apt-get install -y imagemagick
 cd fonts && wget http://www.aeoneducreation.com/~aeon/fonts/webakharslim.ttf && chmod 755 webakharslim.ttf 
 wget http://www.aeoneducreation.com/~aeon/fonts/FreeSerif.ttf && chmod 755 FreeSerif.ttf
 sed -i 's/<policy domain="path" rights="none" pattern=\"@\*\" \/>/<!-- <policy domain="path" rights="none" pattern=\"@\*\" \/> -->/g' /etc/ImageMagick-6/policy.xml
-chmod 755 -R ./*
+chmod 755 -R .
