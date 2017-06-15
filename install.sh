14814830b0c40162d2bb64b5767c21a8eb62b546
iamjagjeetubhi <iamjagjeetubhi@gmail.com>
2017-06-15 13:26:08 +0530

    created font directory

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ fi
 apt-get install -y python-pip && apt-get update
 apt-get install -y python-bs4
 apt-get install -y imagemagick
-cd /usr/share/fonts/truetype/ && wget http://www.aeoneducreation.com/~aeon/fonts/webakharslim.ttf && chmod 755 webakharslim.ttf 
+cd fonts && wget http://www.aeoneducreation.com/~aeon/fonts/webakharslim.ttf && chmod 755 webakharslim.ttf 
 wget http://www.aeoneducreation.com/~aeon/fonts/FreeSerif.ttf && chmod 755 FreeSerif.ttf
 sed -i 's/<policy domain="path" rights="none" pattern=\"@\*\" \/>/<!-- <policy domain="path" rights="none" pattern=\"@\*\" \/> -->/g' /etc/ImageMagick-6/policy.xml
 chmod 755 -R ./*
