5dcadf661e31a0d2a6dfd074599fc0d81f0310f3
iamjagjeetubhi <iamjagjeetubhi@gmail.com>
2017-06-15 14:18:42 +0530

    changed install script

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ fi
 apt-get install -y python-pip && apt-get update
 apt-get install -y python-bs4
 apt-get install -y imagemagick
-cd fonts && wget http://www.aeoneducreation.com/~aeon/fonts/webakharslim.ttf && chmod 755 webakharslim.ttf 
-wget http://www.aeoneducreation.com/~aeon/fonts/FreeSerif.ttf && chmod 755 FreeSerif.ttf
-sed -i 's/<policy domain="path" rights="none" pattern=\"@\*\" \/>/<!-- <policy domain="path" rights="none" pattern=\"@\*\" \/> -->/g' /etc/ImageMagick-6/policy.xml
+cd fonts && chmod 755 webakharslim.ttf && chmod 755 FreeSerif.ttf
+sed -i 's/<policy domain="path" rights="none" pattern=\"@\*\" \/>/<!-- <policy domain="path" rights="none" pattern=\"@\*\" \/> -->/g' /etc/ImageMagick*/policy.xml
 chmod 755 -R .
